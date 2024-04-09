99123a887a4832d96fd6f10f6f968465cd9ec51d
ionutng <ionutngp@gmail.com>
2024-04-09 15:01:16 +0300

    Allow CORS

--- CoffeeTrackerApi/Program.cs
+++ CoffeeTrackerApi/Program.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using CoffeeTrackerApi.Data;
+﻿using CoffeeTrackerApi.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<CoffeeTrackerApiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeTrackerApiContext") ?? throw new InvalidOperationException("Connection string 'CoffeeTrackerApiContext' not found.")));
@@ -11,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowCORS", builder =>
+    {
+        builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -20,6 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowCORS");
 
 app.UseHttpsRedirection();
 
